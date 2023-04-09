1be5e74998c65ae50c3b690aa2f529e6d1d8a33d
nafim-huda <nhuda@protonmail.com>
2023-04-09 17:04:25 -0400

    added redux-persistor to our store

--- huda-clothing/src/index.js
+++ huda-clothing/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { Elements } from '@stripe/react-stripe-js'
 
 
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
 
 import reportWebVitals from './reportWebVitals';
@@ -20,11 +21,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-          <Elements stripe={stripePromise}>
-              <App />
-          </Elements>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+            <Elements stripe={stripePromise}>
+                <App />
+            </Elements>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
